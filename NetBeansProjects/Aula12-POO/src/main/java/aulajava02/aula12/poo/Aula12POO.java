1fdbeda70f0e8c9bd3182ec793de7f43e8c479a6
rodrigo730 <ro783073@gmail.com>
2024-07-12 22:23:06 -0300

    Atualização

---
+++ NetBeansProjects/Aula12-POO/src/main/java/aulajava02/aula12/poo/Aula12POO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package aulajava02.aula12.poo;
+
+/**
+ *
+ * @author rodrigo
+ */
+public class Aula12POO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ NetBeansProjects/Aula12-POO/src/main/java/aulajava02/aula12/poo/Tartaruga.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aulajava02.aula12.poo;
+
+/**
+ *
+ * @author rodrigo
+ */
+public class Tartaruga {
+    
+}
