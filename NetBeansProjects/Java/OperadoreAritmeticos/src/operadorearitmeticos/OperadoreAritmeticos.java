1edc3b375c4671b80f9263d951a3e4498eab064c
rodrigo730 <ro783073@gmail.com>
2024-08-01 17:40:52 -0300

    Atualização

---
+++ NetBeansProjects/Java/OperadoreAritmeticos/src/operadorearitmeticos/OperadoreAritmeticos.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package operadorearitmeticos;
+
+/**
+ *
+ * @author Rodrigo
+ */
+public class OperadoreAritmeticos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /*int n1 = 3;
+        int n2 = 5;
+        float m = (n1 + n2)/2;
+        System.out.println("A media é igual a:" + m);*/
+        
+        float ale = (float) Math.random();
+        int b = (int) (0 + ale * (10-0));
+        System.out.println(ale);
+        System.out.println(b);
+        
+    }
+    
+}
